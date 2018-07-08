feb89996ac788551155d1b24408e4e9d36d5c607
omotyliu <skkif1@gmail.com>
2018-07-08 16:14:29 +0300

    A star algo implementation

--- src/main/java/src/algo/AStar.java
+++ src/main/java/src/algo/AStar.java
@@ -4,6 +4,7 @@ import src.map.PuzzleMap;
 
 import java.util.List;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 
 public class AStar
@@ -13,40 +14,63 @@ public class AStar
 
 	private PuzzleMap res;
 
-	private TreeMap<Integer, PuzzleMap> open = new TreeMap<>();
+	private TreeSet<PuzzleMap> open = new TreeSet<>();
 
-	private TreeMap<Integer, PuzzleMap> closed = new TreeMap<>();
-
-
+	private TreeSet<PuzzleMap> closed = new TreeSet<>();
 
 
-	public void run(PuzzleMap start)
+
+
+	public PuzzleMap run(PuzzleMap start)
 	{
 		finalState = PuzzleMap.getFinalState();
 
 
-		open.put(start.getCoast(), start);
+		open.add(start);
 
 		while (!open.isEmpty())
 		{
-			PuzzleMap currentNode = open.pollFirstEntry().getValue();
+			PuzzleMap currentNode = open.pollFirst();
 
 			if (currentNode.equals(finalState))
 			{
-				res = currentNode;
-				return;
+				return currentNode;
 			}
 
 			List<PuzzleMap> childNodes = currentNode.getPossibleMoves();
 
 			for (PuzzleMap childNode : childNodes)
 			{
-				open.put(childNode.getCoast(), childNode);
+				PuzzleMap temp;
+				if (open.contains(childNode))
+				{
+					temp = open.subSet(childNode, childNode).first();
+
+					if (temp.compareTo(childNode) <= 0)
+					{
+						// skip this node as it is persist in open list with better coast
+						continue;
+					}
+				}
+
+
+				if (closed.contains(childNode))
+				{
+					temp = open.subSet(childNode, childNode).first();
+
+					if (temp.compareTo(childNode) <= 0)
+					{
+						// skip this node as such consist closed list with better coast
+						continue;
+					}
+				}
+
+				open.add(childNode);
 			}
 
-			closed.put(currentNode.getCoast(), currentNode);
+			closed.add(currentNode);
 		}
-
+		return null;
 	}
 
 }
