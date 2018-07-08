2b482270a4cd164394e3c6c7e34d059603567820
omotyliu <skkif1@gmail.com>
2018-07-08 14:57:06 +0300

    a star (recursion stage)

--- src/main/java/src/algo/AStar.java
+++ src/main/java/src/algo/AStar.java
@@ -2,19 +2,51 @@ package src.algo;
 
 import src.map.PuzzleMap;
 
-public class AStar {
+import java.util.List;
+import java.util.TreeMap;
 
-	/*
-	 *   one of the implementation of herustic function
-	 * */
-	IHeuristicFunction heuristicFunction;
 
-	public AStar(IHeuristicFunction heuristicFunction) {
-		this.heuristicFunction = heuristicFunction;
-	}
+public class AStar
+{
 
-	public void run(PuzzleMap start) {
-		System.out.println("A* algo execution with " + heuristicFunction.getClass() + " heuristic function");
+	private PuzzleMap finalState;
+
+	private PuzzleMap res;
+
+	private TreeMap<Integer, PuzzleMap> open = new TreeMap<>();
+
+	private TreeMap<Integer, PuzzleMap> closed = new TreeMap<>();
+
+
+
+
+	public void run(PuzzleMap start)
+	{
+		finalState = PuzzleMap.getFinalState();
+
+
+		open.put(start.getCoast(), start);
+
+		while (!open.isEmpty())
+		{
+			PuzzleMap currentNode = open.pollFirstEntry().getValue();
+
+			if (currentNode.equals(finalState))
+			{
+				res = currentNode;
+				return;
+			}
+
+			List<PuzzleMap> childNodes = currentNode.getPossibleMoves();
+
+			for (PuzzleMap childNode : childNodes)
+			{
+				open.put(childNode.getCoast(), childNode);
+			}
+
+			closed.put(currentNode.getCoast(), currentNode);
+		}
+
 	}
 
 }
