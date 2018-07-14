d4363700a7fc06911aa2e61b2cb5f48f4b826d7d
omotyliu <skkif1@gmail.com>
2018-07-14 13:29:22 +0300

    remove test code for SortedPuzzleSet

--- src/main/java/src/Main.java
+++ src/main/java/src/Main.java
@@ -35,26 +35,5 @@ public class Main
 		res.printParentLine();
 		System.out.println(algo.getExecTime() + " miliseconds");
 
-
-
-//        SortedPuzzleSet test = new SortedPuzzleSet();
-//        PuzzleMap mapClone = new PuzzleMap(map, manager.getMapSize());
-//        mapClone.setHeuristicFunction(new ManhattanDistanceHeuristicFunction());
-//
-//
-//        test.add(initialState);
-//        System.out.println(test.size() == 1);
-//        test.add(initialState);
-//        System.out.println(test.size() == 1);
-//        test.pollFirst();
-//        System.out.println(test.size() == 0);
-//        test.add(initialState);
-//        System.out.println(test.contains(initialState));
-//		test.add(mapClone);
-//		System.out.println(test.get(initialState).getCoast());
-//        System.out.println(test.contains(mapClone));
-//        System.out.println(test.get(mapClone).getCoast());
-
-
     }
 }
