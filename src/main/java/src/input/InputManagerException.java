9f972f4cde6b6d321ef5e1855fd6adaae3a3f5d8
Wismut <dfkmnth55>
2018-07-15 19:14:02 +0300

    Implements arguments management

--- src/main/java/src/input/InputManagerException.java
+++ src/main/java/src/input/InputManagerException.java
@@ -3,6 +3,6 @@ package src.input;
 public class InputManagerException extends RuntimeException {
 
 	public InputManagerException(String message) {
-		super("Bad file : " + message);
+		super("Bad file : " + message); // TODO: 7/15/18 Write input info and exit
 	}
 }
